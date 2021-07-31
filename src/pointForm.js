57b3a871efe888b41edb424b2d693dace88ba7dc
Ian Cargill <ianc@zillowgroup.com>
2021-07-30 20:54:24 -0700

    formatting grid layout

--- src/pointForm.js
+++ src/pointForm.js
@@ -1,17 +1,17 @@
 import { toLocalStorage } from './helpers.js';
-const form = document.querySelector('#point-form');
+const form = document.querySelector('.coordinate form');
 
 function addCustomPoint() {
-  form.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    const title = ev.target.title.value;
-    const lat = ev.target.lat.value;
-    const lon = ev.target.lon.value;
-    const value = { title: title, lat: lat, lon: lon };
-    // console.log(value);
-    toLocalStorage(value);
-    form.reset();
-  });
+	form.addEventListener('submit', (ev) => {
+		ev.preventDefault();
+		const title = ev.target.title.value;
+		const lat = ev.target.lat.value;
+		const lon = ev.target.lon.value;
+		const value = { title: title, lat: lat, lon: lon };
+		// console.log(value);
+		toLocalStorage(value);
+		form.reset();
+	});
 }
 
 export default addCustomPoint();
