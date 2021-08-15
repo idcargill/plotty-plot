5ec72e3c50bcdf177189a5014837efa1d291332b
idcargill <Idcargill@gmail.com>
2021-08-15 12:43:50 -0700

    refactor

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import makeMyPoints from './helpers/makeMyPoints.js';
 import addDmsPoint from './helpers/addDmsPoint.js';
 import fileParser from './fileUpload/fileParser.js';
 import { addPointOnClick } from './helpers/addPointOnClick.js';
-import readUpload from './fileUpload/readUpload.js';
 
 // Add points when clicked on map
 let pointToggle = 0;
 
 // Clear local storage for testing
 document.querySelector('#clear').addEventListener('click', () => {
-	localStorage.clear();
-	console.log('Memory Cleared');
+  localStorage.clear();
+  console.log('Memory Cleared');
 });
 
 // Show My Points Event
@@ -20,19 +19,19 @@ document.querySelector('#show-points').addEventListener('click', makeMyPoints);
 
 // add points on map by clicking
 document.querySelector('#click-point').addEventListener('click', (ev) => {
-	if (pointToggle == 0) {
-		pointToggle = 1;
-		// myMap.on('click', addPointClick);
-		ev.target.textContent = 'ADD Points: ON';
-		ev.target.setAttribute('class', 'on');
-		console.log('on');
-	} else if (pointToggle == 1) {
-		ev.target.textContent = 'ADD Points: OFF';
-		ev.target.setAttribute('class', 'off');
-		pointToggle = 0;
-		// myMap.off('click', myMap, addPointClick);
-		console.log('off');
-	}
+  if (pointToggle == 0) {
+    pointToggle = 1;
+    // myMap.on('click', addPointOnClick);
+    ev.target.textContent = 'ADD Points: ON';
+    ev.target.setAttribute('class', 'on');
+    console.log('on');
+  } else if (pointToggle == 1) {
+    ev.target.textContent = 'ADD Points: OFF';
+    ev.target.setAttribute('class', 'off');
+    pointToggle = 0;
+    // myMap.off('click', myMap, addPointClick);
+    console.log('off');
+  }
 });
 
 // EVENT Add Coordinate Point to storage and map
@@ -47,22 +46,22 @@ dmsBtn.addEventListener('click', addDmsPoint);
 const fileInput = document.querySelector('#file-loader');
 
 fileInput.addEventListener('change', (ev) => {
-	ev.preventDefault();
-	const uploadedFile = fileInput.files[0];
-	fileParser(uploadedFile);
-	// readUpload(uploadedFile);
+  ev.preventDefault();
+  const uploadedFile = fileInput.files[0];
+  fileParser(uploadedFile);
+  // readUpload(uploadedFile);
 });
 
 // EVENT Drop File
 const dropSpot = document.querySelector('.drop-location');
 dropSpot.addEventListener('drop', (ev) => {
-	ev.preventDefault();
-	// document.querySelector('.drop-location').textContent = 'dropped';
-	console.log(ev.dataTransfer.items);
+  ev.preventDefault();
+  // document.querySelector('.drop-location').textContent = 'dropped';
+  console.log(ev.dataTransfer.items);
 });
 
 dropSpot.addEventListener('dragover', (ev) => {
-	ev.preventDefault();
+  ev.preventDefault();
 });
 
 export { myMap };
