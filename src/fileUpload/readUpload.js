70122848e3f7a3db7aa0a74a165eb01e2402732e
idcargill <Idcargill@gmail.com>
2021-08-15 12:31:42 -0700

    testing layers

--- src/fileUpload/readUpload.js
+++ src/fileUpload/readUpload.js
@@ -1,65 +1,65 @@
 import { circleSettings } from '../mapSettings.js';
-import { myMap, baseLayers, overlayMaps, mapControls } from '../mapSetup.js';
+import { myMap, overlayMaps } from '../mapSetup.js';
 
 function findLatLong(row) {
-	const latRegex = /^(lat)|(latitude)/i;
-	const lonRegex = /^(lon)|(longitude)/i;
+  const latRegex = /^(lat)|(latitude)/i;
+  const lonRegex = /^(lon)|(longitude)/i;
 
-	let latIndex = '';
-	let longIndex = '';
+  let latIndex = '';
+  let longIndex = '';
 
-	Object.keys(row).forEach((name, index) => {
-		if (latRegex.test(name)) {
-			// console.log(`lat pass ${name} ${index}`);
-			latIndex = name;
-		}
+  Object.keys(row).forEach((name, index) => {
+    if (latRegex.test(name)) {
+      // console.log(`lat pass ${name} ${index}`);
+      latIndex = name;
+    }
 
-		if (lonRegex.test(name)) {
-			// console.log(`long Pass ${name} ${index}`);
-			longIndex = name;
-		}
-	});
-	return { lat: latIndex, lon: longIndex };
+    if (lonRegex.test(name)) {
+      // console.log(`long Pass ${name} ${index}`);
+      longIndex = name;
+    }
+  });
+  return { lat: latIndex, lon: longIndex };
 }
 
 // Read uploaded file
 function readUpload(fileLoaded) {
-	const fileName = fileLoaded.name;
-	if (myMap.fileControl) {
-		myMap.removeControl(fileControl);
-	}
-	const groupArr = [];
-	let reader = new FileReader();
-	reader.onload = (x) => {
-		const data = JSON.parse(x.target.result);
-		// console.log(data);
+  const fileName = fileLoaded.name;
+  if (myMap.fileControl) {
+    myMap.removeControl(fileControl);
+  }
+  const groupArr = [];
+  let reader = new FileReader();
+  reader.onload = (x) => {
+    const data = JSON.parse(x.target.result);
+    // console.log(data);
 
-		// Find Lat/lon indexes in uploaded file
-		const dataArr = Object.values(data);
-		const coordIndexLookup = findLatLong(data[0]);
+    // Find Lat/lon indexes in uploaded file
+    const dataArr = Object.values(data);
+    const coordIndexLookup = findLatLong(data[0]);
 
-		// Add Points to map uisng the lat/long Regex tested values
-		// array of Objects
-		dataArr.forEach((i) => {
-			const latitude = i[coordIndexLookup.lat];
-			const longitude = i[coordIndexLookup.lon];
+    // Add Points to map uisng the lat/long Regex tested values
+    // array of Objects
+    dataArr.forEach((i) => {
+      const latitude = i[coordIndexLookup.lat];
+      const longitude = i[coordIndexLookup.lon];
 
-			const circle = L.circle([latitude, longitude], circleSettings);
-			// Popup text from JSON file data
-			circle.bindPopup(JSON.stringify(i));
-			groupArr.push(circle);
-		});
-		// Add Group to map
-		// const fileData = L.featureGroup(groupArr).addTo(myMap);
+      const circle = L.circle([latitude, longitude], circleSettings);
+      // Popup text from JSON file data
+      circle.bindPopup(JSON.stringify(i));
+      groupArr.push(circle);
+    });
+    // Add Group to map
+    // const fileData = L.featureGroup(groupArr).addTo(myMap);
 
-		const newLayer = L.layerGroup(groupArr);
-		Object.assign(overlayMaps, { [fileName]: newLayer });
-		const newLayerControl = L.control.layers(overlayMaps);
-		// Add layer to map
-		myMap.addLayer(newLayer);
-		newLayerControl.addTo(myMap);
-	};
-	reader.readAsText(fileLoaded);
+    const newLayer = L.layerGroup(groupArr);
+    Object.assign(overlayMaps, { [fileName]: newLayer });
+    const newLayerControl = L.control.layers(overlayMaps);
+    // Add layer to map
+    myMap.addLayer(newLayer);
+    newLayerControl.addTo(myMap);
+  };
+  reader.readAsText(fileLoaded);
 }
 
 export default readUpload;
--- src/mapSetup.js
+++ src/mapSetup.js
@@ -1,30 +1,62 @@
 // Public access Token
 const token =
-	'pk.eyJ1IjoiaWRjYXJnaWxsIiwiYSI6ImNrcmh1MGY3bTBvaHgydm80ejhkcjVoMjEifQ.Kzjr5NN2uxIGUBGzgMteLg';
+  'pk.eyJ1IjoiaWRjYXJnaWxsIiwiYSI6ImNrcmh1MGY3bTBvaHgydm80ejhkcjVoMjEifQ.Kzjr5NN2uxIGUBGzgMteLg';
 
 // Map loads into DOM id (needs height)
 const myMap = L.map('leafletmap').setView([51.505, -0.09], 5);
 
-const baseMap = L.tileLayer(
-	'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-	{
-		attribution:
-			'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		maxZoom: 18,
-		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1,
-		accessToken: token,
-	}
+// Base World Map
+// const baseMap = L.tileLayer(
+//   'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+//   {
+//     attribution:
+//       'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: token,
+//   }
+// ).addTo(myMap);
+
+// Satalite Map
+const satalite = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: token,
+  }
 ).addTo(myMap);
 
-// Map Layers Obj.
-const baseLayers = {
-	base: baseMap,
+// TOPO Map
+const outdoors = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/outdoors-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: token,
+  }
+).addTo(myMap);
+
+// Base Maps Layers
+const baseMaps = {
+  topo: outdoors,
+  satalite: satalite,
 };
 
+// Empty Overlay container
 const overlayMaps = {};
 
-const mapControls = L.control.layers(baseLayers).addTo(myMap);
+// Initialize Map Controls
+L.control.layers(baseMaps).addTo(myMap);
 
-export { myMap, baseLayers, overlayMaps, mapControls };
+export { myMap, overlayMaps };
