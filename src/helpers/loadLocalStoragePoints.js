13a1e0b1aaa26130ad6015b89808067128588877
Ian Cargill <ianc@zillowgroup.com>
2021-09-06 07:57:01 -0700

    eslint

--- src/helpers/loadLocalStoragePoints.js
+++ src/helpers/loadLocalStoragePoints.js
@@ -1,24 +1,25 @@
 // Load points from local Stoarage
 // Points loaded on page load.
 import { pointSettings } from '../map/mapSettings.js';
-import { myMap, overlayMaps } from '../map/mapSetup.js';
+import { overlayMaps } from '../map/mapSetup.js';
 
 const loadLocalStoragePoints = () => {
 	const myPoints = localStorage.getItem('myPoints');
 
 	if (myPoints) {
 		const parsedPoints = JSON.parse(myPoints);
+		console.log(parsedPoints);
 
-		for (let point in parsedPoints) {
-			const title = parsedPoints[point].Title;
-			const lat = parsedPoints[point].lat;
-			const lon = parsedPoints[point].lon;
+		parsedPoints.forEach((point) => {
+			const title = point.Title;
+			const lat = point.lat;
+			const lon = point.lon;
 
 			const mapPoint = L.circle([lat, lon], pointSettings);
 			mapPoint.bindPopup(title);
 			let newLayer = L.layerGroup([mapPoint]);
 			overlayMaps.myPoints.addLayer(newLayer);
-		}
+		});
 	} else {
 		console.log('local storage is empty');
 	}
