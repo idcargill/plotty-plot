a5536c85b928d59528da1c9e43168c1cc7c5c244
Ian Cargill <ianc@zillowgroup.com>
2021-09-06 15:26:26 -0700

    w

--- src/helpers/clearHistory.js
+++ src/helpers/clearHistory.js
@@ -1,15 +1,12 @@
 // Clear local history and map points
 
-import { myMap, overlayMaps } from '../map/mapSetup.js';
+import { overlayMaps } from '../map/mapSetup.js';
 
-function clearLocalHistory(e) {
+function clearLocalHistory() {
 	if (confirm('Would you like to delete your data points?')) {
 		localStorage.clear();
 		// myMap.removeLayer(overlayMaps.myPoints);
 		overlayMaps.myPoints.remove();
-		console.log(localStorage);
-		console.log(overlayMaps.myPoints);
-		e.target.textContent = 'Cleared';
 	}
 }
 
--- src/helpers/toLocalStorage.js
+++ src/helpers/toLocalStorage.js
@@ -1,36 +1,40 @@
 // takes in object
 function toLocalStorage(x) {
-	const storage = [];
-	const myPoints = localStorage.getItem('myPoints');
-	// console.log(myPoints);
+	if (!x.lat | !x.lon) {
+		console.log('no coordinates included');
+	} else {
+		const storage = [];
+		const myPoints = localStorage.getItem('myPoints');
+		// console.log(myPoints);
 
-	if (myPoints) {
-		const parsedPoints = JSON.parse(myPoints);
+		if (myPoints) {
+			const parsedPoints = JSON.parse(myPoints);
 
-		// Pull array objs and reload storage arr
-		if (parsedPoints.length > 0) {
-			parsedPoints.forEach((i) => {
-				storage.push(i);
-			});
+			// Pull array objs and reload storage arr
+			if (parsedPoints.length > 0) {
+				parsedPoints.forEach((i) => {
+					storage.push(i);
+				});
 
-			// Only 1 storage object
-		} else if (parsedPoints) {
-			storage.push(parsedPoints);
+				// Only 1 storage object
+			} else if (parsedPoints) {
+				storage.push(parsedPoints);
+			}
+
+			// Load new object into storage arr
+			storage.push(x);
+			console.log(storage);
+
+			// Load Local storage wtih array of objects
+			const updatedString = JSON.stringify(storage);
+			localStorage.setItem('myPoints', updatedString);
+
+			// First item in storage
+		} else if (!myPoints) {
+			storage.push(x);
+			const updatedString = JSON.stringify(x);
+			localStorage.setItem('myPoints', updatedString);
 		}
-
-		// Load new object into storage arr
-		storage.push(x);
-		console.log(storage);
-
-		// Load Local storage wtih array of objects
-		const updatedString = JSON.stringify(storage);
-		localStorage.setItem('myPoints', updatedString);
-
-		// First item in storage
-	} else if (!myPoints) {
-		storage.push(x);
-		const updatedString = JSON.stringify(x);
-		localStorage.setItem('myPoints', updatedString);
 	}
 }
 
